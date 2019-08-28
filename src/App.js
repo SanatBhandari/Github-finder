805ea0ddf28da29de6e56e205f34d2c513823859
sbhandari <sanatbhandari33@gmail.com>
2019-08-28 14:03:47 -0500

    Creating grid and setting states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
+import axios from 'axios';
+import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
